7aa1f2a7073f76e4fa63fe323cf2fdace747aa0b
Jerin Abraham <abrahamjerin98@gmail.com>
2025-11-20 21:42:52 -0500

    Recreate project
    - Change CubeMX settings so that only necessary library files are
      included to reduce folder clutter
    - Change project name to match the name of the board (Open_PWM_Gyro)
    - Refactor app_main (now app)

---
+++ Core/Inc/app.h
@@ -0,0 +1,28 @@
+/*!
+  @file   app.h
+  @brief  Main user code implementation to avoid the ST generated comments
+
+  Instead of deleting all the USER CODE comments in the main.c provided by ST,
+  the actual application code will be here and the config() and loop() functions
+  will be called in the ST provided main.c file
+
+  This preserves the main.c code and keeps the application code from being deleted
+  if code generation with CubeMX is ever needed
+*/
+#ifndef APP_H
+#define APP_H
+
+#include "stm32g4xx_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void config(void);
+void loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,12 +18,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "usb_device.h"
+#include "usb.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "app_main.h"
-#include <stdint.h>
+#include "app.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -49,7 +49,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -88,16 +87,16 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USB_Device_Init();
+  MX_USB_PCD_Init();
   /* USER CODE BEGIN 2 */
-  app_config();
+  config();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    app_loop();
+    loop();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -149,26 +148,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
