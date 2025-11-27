0aba26143179bf688e6a0370ea2f3ca4f32d6e63
Jerin Abraham <abrahamjerin98@gmail.com>
2025-11-27 10:07:39 -0500

    Add rx buffer preloading for debug
    - Read 128 words from the UART FIFO before processing it so I can see
    where the decoding is falling apart

--- Lib/App/Src/app.c
+++ Lib/App/Src/app.c
@@ -16,6 +16,13 @@
 void config() {
   // Initialize peripherals
   receiver_init();
+
+  process_receiver();
+  if (rx.rx_data_valid) {
+    rx.rx_data_valid = 0;
+    // HAL_GPIO_TogglePin(LED_RED_PORT, LED_RED_PIN);
+    // CDC_Transmit_FS(rx.rx_data, 36);
+  }
 }
 
 /*!
@@ -25,20 +32,13 @@ void config() {
 */
 void loop() {
   // Process peripheral data
-  process_receiver();
-  if (rx.rx_data_valid) {
-    rx.rx_data_valid = 0;
-    // uint8_t tx_buf[33];
-    // for (int i = 0; i < 16; i++) {
-    //   tx_buf[2 * i + 0] = (uint8_t) rx.rx_data[i] & 0xFF;
-    //   tx_buf[2 * i + 1] = (uint8_t) (rx.rx_data[i] >> 8) & 0xFF;
-    // }
-    // tx_buf[32] = '\n';
-    // CDC_Transmit_FS(tx_buf, 33);
-    uint8_t tx_buf[64];
-    uint8_t tx_buf_len = snprintf((char *) tx_buf, 64, "RX data valid %lu\r\n", HAL_GetTick());
-    CDC_Transmit_FS(tx_buf, tx_buf_len);
-  }
+  // process_receiver();
+  // if (rx.rx_data_valid) {
+  //   rx.rx_data_valid = 0;
+  //   uint8_t tx_buf[64];
+  //   uint8_t tx_buf_len = snprintf((char *) tx_buf, 64, "RX data valid %lu\r\n", HAL_GetTick());
+  //   CDC_Transmit_FS(tx_buf, tx_buf_len);
+  // }
   // uint8_t tx_buf[64];
   // uint8_t tx_buf_len = snprintf((char*) tx_buf, 64, "%lu\r\n", HAL_GetTick());
   // CDC_Transmit_FS(tx_buf, tx_buf_len);
--- Lib/Receiver/Src/receiver.c
+++ Lib/Receiver/Src/receiver.c
@@ -8,7 +8,7 @@ receiver_t rx = {
   .rx_data_valid = 0
 };
 
-uint8_t rx_buffer[32];
+uint8_t rx_buffer[64];
 
 // ----------------------------------------------------------------------
 // PRIVATE FUNCTIONS
@@ -22,16 +22,16 @@ static inline void parse_ibus_data(uint8_t data) {
     WAIT_FOR_CHECKSUM_2
   } state = WAIT_FOR_LENGTH;
 
-  static uint8_t packet_len = 0;
+  static uint8_t payload_len = 0;
   static uint16_t checksum_calc = 0;
-  static uint16_t checksum_val = 0;
+  static uint16_t checksum_rx = 0;
   static uint8_t buf_i = 0;
 
   switch (state) {
     case WAIT_FOR_LENGTH: {
       if (data > 0x03 && data <= 0x20) {
         buf_i = 0;
-        packet_len = data - 3;
+        payload_len = data + 1; // n bytes of data + 1 cmd byte
         checksum_calc = 0xFFFF - data;
         state = WAIT_FOR_DATA;
       }
@@ -41,21 +41,21 @@ static inline void parse_ibus_data(uint8_t data) {
     case WAIT_FOR_DATA: {
       rx_buffer[buf_i++] = data;
       checksum_calc -= data;
-      if (buf_i == packet_len) {
+      if (buf_i == payload_len) {
         state = WAIT_FOR_CHECKSUM_1;
       }
       break;
     }
 
     case WAIT_FOR_CHECKSUM_1: {
-      checksum_val |= (uint16_t) data;
+      checksum_rx |= (uint16_t) data;
       state = WAIT_FOR_CHECKSUM_2;
       break;
     }
 
     case WAIT_FOR_CHECKSUM_2: {
-      checksum_val |= (uint16_t) data << 8;
-      if (checksum_calc == checksum_val) {
+      checksum_rx |= (uint16_t) data << 8;
+      if (checksum_calc == checksum_rx) {
         uint8_t cmd = rx_buffer[0];
         if (cmd == 0x40) {
           for (int i = 1; i < 16; i++) {
@@ -88,8 +88,17 @@ void receiver_init() {
   @brief Read bytes
 */
 void process_receiver() {
-  if (!usart1_rx_fifo_empty()) {
-    uint8_t data = usart1_read_rx_fifo();
-    process_received_byte(data);
+  uint8_t rx_buf[128];
+  uint8_t rx_buf_i = 0;
+  // Preload data into buffer
+  while (rx_buf_i < 128) {
+    if (!usart1_rx_fifo_empty()) {
+      uint8_t data = usart1_read_rx_fifo();
+      rx_buf[rx_buf_i++] = data;
+    }
+  }
+  // Process rx data
+  for (int i = 0; i < 128; i++) {
+    process_received_byte(rx_buf[i]);
   }
 }
