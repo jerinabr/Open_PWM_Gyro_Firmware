9dacfa003787c9f244eea04856a02f989461dc3b
Jerin Abraham <abrahamjerin98@gmail.com>
2025-11-28 12:20:51 -0500

    Add USART reconfiguration functionality
    - USART can be reconfigured with a different baud rate, parity bit,
      stop bit, or rx logic level inversion
    - This will be used for reconfiguring the receiver based on a specific
      rx protocol

--- Lib/Receiver/Inc/usart1.h
+++ Lib/Receiver/Inc/usart1.h
@@ -12,7 +12,40 @@
 extern "C" {
 #endif
 
-void usart1_init(void);
+// ----------------------------------------------------------------------
+// TYPES
+// ----------------------------------------------------------------------
+
+/*!
+  @brief USART1 configuration data
+*/
+typedef struct {
+  uint32_t baud_rate; /*!< Baud rate must be > 0 and <= 10,000,000 */
+  uint8_t idle_level; /*!< Indicates if the UART idle logic level is low or high
+                            0 - Idle logic level is low
+                            1 - Idle logic level is high */
+  uint8_t parity;     /*!< Odd, even, or no parity bit
+                            0 - No parity bit
+                            1 - Odd parity bit
+                            2 - Even parity bit */
+  uint8_t stop_bits;  /*!< 1 stop bit or 2 stop bits
+                            1 - 1 stop bit
+                            2 - 2 stop bits */
+} usart1_config_t;
+
+// ----------------------------------------------------------------------
+// CONSTANTS
+// ----------------------------------------------------------------------
+
+extern const usart1_config_t usart1_default_config;
+
+// ----------------------------------------------------------------------
+// FUNCTIONS
+// ----------------------------------------------------------------------
+
+void usart1_init(const usart1_config_t *config_data);
+void usart1_reconfig(const usart1_config_t *config_data);
+
 uint8_t usart1_rx_fifo_empty(void);
 uint8_t usart1_read_rx_fifo(void);
 
--- Lib/Receiver/Src/usart1.c
+++ Lib/Receiver/Src/usart1.c
@@ -5,13 +5,23 @@
 #include <stdint.h>
 
 #define USART1_KER_CK_PRES    (160E6f)
-#define STARTUP_BAUD          (115200)
 
 #define GPIO_PA9_AF7_USART1   (0x7UL << GPIO_AFRH_AFSEL9_Pos)
 #define GPIO_PA9_AF_MODE      (0x2UL << GPIO_MODER_MODE9_Pos)
 
 #define GPIO_PA10_AF7_USART1  (0x7UL << GPIO_AFRH_AFSEL10_Pos)
 #define GPIO_PA10_AF_MODE     (0x2UL << GPIO_MODER_MODE10_Pos)
+
+// ----------------------------------------------------------------------
+// CONSTANTS
+// ----------------------------------------------------------------------
+
+const usart1_config_t usart1_default_config = {
+  .baud_rate = 115200,
+  .idle_level = 1,
+  .parity = 0,
+  .stop_bits = 1
+};
 
 // ----------------------------------------------------------------------
 // PRIVATE FUNCTIONS
@@ -23,7 +33,7 @@
 
   This baud rate calculation is only applicable when OVER8=0
 */
-static inline uint32_t calculate_brr(unsigned int baud) {
+static inline uint32_t calculate_brr(uint32_t baud) {
   return (uint32_t) roundf(USART1_KER_CK_PRES / baud) & 0xFFFF;
 }
 
@@ -49,29 +59,113 @@ static void configure_pins(void) {
 /*!
   @brief Configure USART1 to receive in FIFO mode
 */
-static void configure_usart1(void) {
+static void configure_usart1(const usart1_config_t *config_data) {
   // Enable clock for USART1
   SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART1EN);
 
-  // Disable USART1 before configuration
-  CLEAR_BIT(USART1->CR1, USART_CR1_UE);
-
-  // Configure USART1
-  SET_BIT(USART1->BRR,  calculate_brr(STARTUP_BAUD)); // Set baud rate to default baud rate
-  SET_BIT(USART1->CR1,  USART_CR1_FIFOEN |            // Enable FIFOs
-                        USART_CR1_RE |                // Enable receiver
-                        USART_CR1_UE);                // Enable USART
+  // Configure USART1 with desired configuration
+  usart1_reconfig(config_data);
 }
 // ----------------------------------------------------------------------
 // PUBLIC FUNCTIONS
 // ----------------------------------------------------------------------
 
 /*!
-  @brief Initialize the USART1 peripheral
+  @brief Initialize the USART1 peripheral with specified configuration
 */
-void usart1_init() {
+void usart1_init(const usart1_config_t *config_data) {
   configure_pins();
-  configure_usart1();
+  configure_usart1(config_data);
+}
+
+/*!
+  @brief Reconfigure USART1 with the specified configuration
+*/
+void usart1_reconfig(const usart1_config_t *config_data) {
+  // Disable USART1 before reconfiguration
+  CLEAR_BIT(USART1->CR1, USART_CR1_UE);
+
+  /*
+    Validate specified configuration
+  */
+  const usart1_config_t *selected_config;
+  // USART1 is configured to oversample by 16 so max baud rate is USART1_KER_CK_PRES / 16
+  if (
+    (config_data->baud_rate > 0 && config_data->baud_rate <= (USART1_KER_CK_PRES / 16)) &&
+    (config_data->idle_level <= 1) &&
+    (config_data->parity <= 2) &&
+    (config_data->stop_bits > 0 && config_data->stop_bits <= 2)
+  ) {
+    selected_config = config_data;
+  }
+  // If given configuration is invalid then use default configuration
+  else {
+    selected_config = &usart1_default_config;
+  }
+
+  /*
+    Select bit fields based on selected configuration
+  */
+  // Calculate brr value (register BRR)
+  uint32_t brr_val = calculate_brr(selected_config->baud_rate);
+  // Select rxinv bit field (register CR2)
+  uint8_t rxinv_val = selected_config->idle_level == 0 ? 1 : 0;
+  // Select parity bit fields (register CR1)
+  uint8_t pce_val;
+  uint8_t ps_val;
+  switch (config_data->parity) {
+    case 0:
+      pce_val = 0;
+      ps_val = 0;
+      break;
+    case 1:
+      pce_val = 1;
+      ps_val = 1;
+      break;
+    case 2:
+      pce_val = 1;
+      ps_val = 0;
+      break;
+    default:
+      pce_val = 0;
+      ps_val = 0;
+      break;
+  }
+  // Select stop bit fields (register CR2)
+  uint8_t stop_val;
+  switch (config_data->stop_bits) {
+    case 1:
+      stop_val = 0b00;
+      break;
+    case 2:
+      stop_val = 0b10;
+      break;
+    default:
+      stop_val = 0b00;
+      break;
+  }
+
+  /*
+    Configure USART1 registers
+  */
+  MODIFY_REG( // Set baud rate
+    USART1->BRR,
+    USART_BRR_BRR_Msk,
+    brr_val
+  );
+  MODIFY_REG( // Set rx inverted control and stop bits
+    USART1->CR2,
+    USART_CR2_RXINV_Msk | USART_CR2_STOP_Msk,
+    (rxinv_val << USART_CR2_RXINV_Pos) | (stop_val << USART_CR2_STOP_Pos)
+  );
+  MODIFY_REG( // Set parity control bits
+    USART1->CR1,
+    USART_CR1_PCE_Msk | USART_CR1_PS_Msk,
+    (pce_val << USART_CR1_PCE_Pos) | (ps_val << USART_CR1_PS_Pos)
+  );
+  SET_BIT(USART1->CR1,  USART_CR1_FIFOEN |  // Enable FIFOs
+                        USART_CR1_RE |      // Enable receiver
+                        USART_CR1_UE);      // Enable USART
 }
 
 /*!
